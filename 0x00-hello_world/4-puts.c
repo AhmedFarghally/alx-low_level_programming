599f466172ee01fb4b84486adb80816212301c5b
AhmedFarghally <ahmedfarghally27@gmail.com>
2023-03-16 01:21:09 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * main -  program to print a sentence
+  *
+  * Return:  main always returns 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
