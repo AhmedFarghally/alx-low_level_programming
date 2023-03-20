b18c247d00165ba1ba7736ace7073a3d31177c63
AhmedFarghally <ahmedfarghally27@gmail.com>
2023-03-20 06:13:10 -0700

    4. When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - start point
+ * Return: Always 0
+ */
+int main(void)
+{
+	/*Variables Declaration*/
+	int n;
+	/**/
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	for (n = 0; n <= 9; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
