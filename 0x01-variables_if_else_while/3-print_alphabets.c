eb2c03045ed6d4f75e68b94c132d2b2102572e4a
AhmedFarghally <ahmedfarghally27@gmail.com>
2023-03-20 05:56:32 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - start point
+ * Return: Always 0
+ */
+int main(void)
+{
+	char n;
+	char m;
+	/*Assignment*/
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	for (n = 'a'; n <= 'z'; n++)
+		putchar(n);
+	for (m = 'A'; m <= 'Z'; m++)
+		putchar(m);
+
+	return (0);
+}
