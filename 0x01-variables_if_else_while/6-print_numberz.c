5594914e0e432555226698c243a430288c471190
AhmedFarghally <ahmedfarghally27@gmail.com>
2023-03-20 06:53:39 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - start point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+	/**/
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	for (n =48; n <= 57; n++)
+		putchar((char) n);
+	printf("\n");
+	return (0);
+}
