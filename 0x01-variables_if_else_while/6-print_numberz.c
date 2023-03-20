8297c4534ddaf9673d89d4679bfd19a65d753fed
AhmedFarghally <ahmedfarghally27@gmail.com>
2023-03-20 07:04:40 -0700

    6. Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -13,7 +13,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	for (n =48; n <= 57; n++)
+	for (n = 48; n <= 57; n++)
 		putchar((char) n);
 	putchar('\n');
 	return (0);
