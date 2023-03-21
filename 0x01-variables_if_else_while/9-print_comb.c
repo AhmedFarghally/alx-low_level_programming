13205020060620658da09f36d7da0368801ca0d2
AhmedFarghally <ahmedfarghally27@gmail.com>
2023-03-20 17:02:05 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - start point
+ * Return: Always 0
+ */
+int main(void)
+{
+	/*Var Declaration*/
+	int n;
+	/**/
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	for (n = 48; n <= 57; n++)
+	{
+		putchar((char) n);
+		putchar(',');
+		putchar(' ');
+	}
+	printf("\n");
+	return (0);
+}
