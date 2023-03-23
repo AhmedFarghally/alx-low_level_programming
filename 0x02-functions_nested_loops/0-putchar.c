e44a6ec411155c76175b83ce19f8af03a6a63141
AhmedFarghally <ahmedfarghally27@gmail.com>
2023-03-22 20:02:49 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+/* more headers goes there */
+#include "main.h"
+/**
+ * main - start point
+ * Return: Always 0
+ */
+int main(void)
+{
+	/* your code goes there */
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
