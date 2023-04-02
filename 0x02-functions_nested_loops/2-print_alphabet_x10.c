69734b6cd0be2b8a126b9b05f65ea195d2670764
AhmedFarghally <ahmedfarghally27@gmail.com>
2023-04-01 18:40:27 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * print_alphabet - This function is used to print alphabets in lower case
+  * print_alphabet_x10 - This function is used to print alphabets in lower case
   * Return: Always 0
   */
 
@@ -15,7 +15,7 @@ void print_alphabet_x10(void)
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
